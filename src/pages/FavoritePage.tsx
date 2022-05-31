f42ab156c02a3def8d09128936ed693644ece557
Pablo del Barrio <katatonio@hotmail.com>
2022-05-31 10:16:16 +0200

    Add to favorites functionality implemented

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,6 +1,64 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/auth.context";
 import { FavoritesContainer } from "../ui/FavoritesContainer";
 
+//FALTA TIPAR TODO:
+
+export type FavoriteSpots = {
+  spotID: string;
+  spotName: string;
+  spotImage: string;
+  spotUrl: string;
+};
+
 function FavoritePage() {
+  const [favoritedSpots, setFavoritedSpots] = useState<FavoriteSpots[]>([]);
+  const { user } = useContext(AuthContext);
+
+  const variable = { userFrom: user?._id };
+
+  useEffect(() => {
+    const authAxios = axios.create({
+      baseURL: import.meta.env.VITE_API_BASE_URL as string,
+      withCredentials: true,
+    });
+
+    const getFavoriteSpots = async () => {
+      console.log(variable);
+      await authAxios
+        .post("/favorites/getfavoritedspots", variable)
+        .then((response) => {
+          if (response.data.success) {
+            setFavoritedSpots(response.data.favorites);
+          } else {
+            alert("Failed to get favorited spots");
+          }
+        });
+    };
+    getFavoriteSpots().catch(console.error);
+    //
+  }, []);
+
+  const renderTableBody = favoritedSpots.map((spot, index) => {
+    return (
+      <tr>
+        <td>{spot.spotName}</td>
+        <td>
+          <img
+            style={{ width: "100px" }}
+            src={spot.spotImage}
+            alt={spot.spotID}
+          />
+        </td>
+        <td>
+          <a href={spot.spotUrl}>{spot.spotUrl}</a>
+        </td>
+        <td>Remove</td>
+      </tr>
+    );
+  });
+
   return (
     <FavoritesContainer>
       <h3>My Favorite Spots</h3>
@@ -15,7 +73,7 @@ function FavoritePage() {
           </tr>
         </thead>
 
-        <tbody></tbody>
+        <tbody>{renderTableBody}</tbody>
       </table>
     </FavoritesContainer>
   );
