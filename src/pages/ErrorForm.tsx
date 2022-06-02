50d328a7f33454b0f901d56eca255b0f1005e280
Pablo del Barrio <katatonio@hotmail.com>
2022-06-02 18:58:37 +0200

    comented gif import in error page

--- src/pages/ErrorForm.tsx
+++ src/pages/ErrorForm.tsx
@@ -1,10 +1,10 @@
-import colau from "/colau404.gif";
+// import colau from "/colau404.gif";
 import { Error, ErrorContainer } from "../ui/Error";
 
 const ErrorForm = () => {
   return (
     <ErrorContainer>
-      <img className="error" src={colau} alt="Error 404" />
+      {/* <img className="error" src={colau} alt="Error 404" /> */}
       <Error size="l">404 | Not Found</Error>
     </ErrorContainer>
   );
